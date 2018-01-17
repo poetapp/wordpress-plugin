a790434785fe82df845a55d99526a0122f70f10a
Mostafa <mostafa@mostafakhater.com>
2018-01-17 20:55:12 +0200

    Updating consumer POST request to create Work through Frost API

--- PoetArticlePoster/Consumer.php
+++ PoetArticlePoster/Consumer.php
@@ -15,8 +15,7 @@ class Consumer {
 	 * Consumer constructor.
 	 *
 	 * @param $url
-	 * @param $public_key
-	 * @param $private_key
+	 * @param token
 	 * @param $post
 	 */
 	public function __construct( $url, $token, $post ) {
@@ -28,15 +27,40 @@ class Consumer {
 	/**
 	 * Consume method
 	 * The main method used to send articles to Po.et API
-	 * @TODO body still not complete
 	 * @return array|WP_Error
 	 */
 	public function consume() {
-		$response = wp_remote_post( $this->url . '/user/claims', array(
+
+        $user       = get_user_by( 'ID', $this->post->post_author );
+        $tags_array = wp_get_post_tags( $this->post->ID, array( 'fields' => 'names' ) );
+        $tags       = implode(',', $tags_array);
+        $author     = $user->display_name;
+
+        if ( ! empty( $user->first_name ) ) {
+            $author = $user->first_name;
+            if ( ! empty( $user->last_name ) ) {
+                $author .= ' ' . $user->last_name;
+            }
+        } else if ( ! empty( $user->last_name ) ) {
+            $author = $user->last_name;
+        }
+
+        $body_array = array(
+	        'name'          => $this->post->post_title,
+            'datePublished' => get_the_modified_time( 'c', $this->post ),
+            'dateCreated'   => get_the_time( 'c', $this->post ),
+            'author'        => $author,
+            'tags'          => $tags,
+            'content'       => $this->post->post_content
+        );
+	    $body_json = wp_json_encode( $body_array );
+
+		$response = wp_remote_post( $this->url, array(
 				'method'  => 'POST',
-				'timeout' => 1000,
-				'headers' => array( 'content-type' => 'text/plain' ),
-				'body'    => '',
+				'timeout' => 30,
+				'headers' => array( 'Content-Type'  => 'application/json',
+                                    'token'         => $this->token ),
+				'body'    => $body_json,
 			)
 		);
 
