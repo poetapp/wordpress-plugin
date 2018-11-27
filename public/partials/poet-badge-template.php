248ee17bf2d3dbfcf0fd36b229272b86e4d12ace
Lautaro Dragan <lautaro.dragan@gmail.com>
2018-11-27 16:32:48 -0300

    Update badge link (#47)

--- public/partials/poet-badge-template.php
+++ public/partials/poet-badge-template.php
@@ -18,7 +18,7 @@ function print_poet_template( $quill_image_url, $work_id, $display_date ) {
 	ob_start()
 	?>
 	<div class = "poet-container">
-		<a href="https://explorer.po.et/works/<?php echo esc_html( $work_id ); ?>" target="_blank">
+		<a href="https://explorer-mainnet.poetnetwork.net/works/<?php echo esc_html( $work_id ); ?>" target="_blank">
 			<div class = "poet-inner">
 				<img src="<?php echo esc_attr( $quill_image_url ); ?>" class = "poet-image" >
 				<div>
